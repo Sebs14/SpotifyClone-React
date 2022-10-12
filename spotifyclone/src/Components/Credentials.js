e4597894dbd6e98986448cbee5b41419a6da900f
andreayalad <alejandrayala01@gmail.com>
2022-10-11 22:29:25 -0600

    :sparkles: Add Credentials
    Adding credentials auth and token validation

---
+++ spotifyclone/src/Components/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '906af4271ac44f3e8f119913d9c9d0f1',
+        ClientSecret: '28a17a7be1e44a75aeef99c441078e12'
+    }
+}
+
+export { Credentials };
